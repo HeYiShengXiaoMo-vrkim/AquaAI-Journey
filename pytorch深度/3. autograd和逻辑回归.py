e092faddcd3af1988196088c29b6dacf2b39c83a
Aqua-vrkim <142286999+HeYiShengXiaoMo-vrkim@users.noreply.github.com>
2025-03-14 10:13:00 +0800

    Update 3. autograd和逻辑回归.py

--- pytorch深度/3. autograd和逻辑回归.py
+++ pytorch深度/3. autograd和逻辑回归.py
@@ -7,4 +7,8 @@ import numpy
 ~   torch.autograd.backward(tensors, grad_tensors=None, retain_graph=None, create_graph=False)
     功能 : 自动求导的张量,如loss
     tensors : 用来自动求导的张量
+    retain_graph: 保存计算图
+    create_graph: 创建导数计算图
+    grad_tensors: 多梯度权重
+
 """
